d3d6d90257bf1cf6b8cb857c613621cf236d9238
Ty Sand <sandersontyrone77@gmail.com>
2021-12-04 13:55:34 -0500

    deleted file

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-# VPC
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/18"
-
-  tags = {
-    Name = "Main VPC"
-  }
-}
